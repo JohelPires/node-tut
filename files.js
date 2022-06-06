0e77c81243b31c0fffdadd2865273a2c9c798e71
JohelPires <johelpires@gmail.com>
2022-06-05 22:18:50 -0400

    write files and folders

--- files.js
+++ files.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
 
 
-fs.readFile('./docs/post01.md', (err, data) => {
+// fs.readFile('./docs/post01.md', (err, data) => {
 
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data.toString())
+
+// })
+
+// fs.writeFile('./docs/post02.md', '# This is the second blog post', () => {
+//     console.log('post02 saved.')
+// })
+
+if (!fs.existsSync('./assets')) {
+fs.mkdir('./assets', (err) => {
     if (err) {
         console.log(err)
     }
-    console.log(data.toString())
-
-})
+    console.log('folder created')
+})}

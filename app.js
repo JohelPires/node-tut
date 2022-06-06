b5b92a1975465b4e135f67f05fbcb5db63bd7281
JohelPires <johelpires@gmail.com>
2022-06-06 11:00:53 -0400

    setting up ejs views

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express')
 // express app
 const app = express()
 
+// registrar view engine
+app.set('view engine', 'ejs')
+
+
 // listen for requestes
 app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname})
+    res.render('index')
 })
 app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', { root: __dirname})
